1c909bfc80009ab9eabc5045c4a1ade0382b9420
Jiayu Liu <jimexist@gmail.com>
2015-01-14 19:36:57 -0500

    some cleanup

---
+++ baseline/tiny.h
@@ -0,0 +1,15 @@
+#ifndef _TINY_H_
+#define _TINY_H_
+
+#include "csapp.h"
+
+void handle_request(int fd);
+void read_requesthdrs(rio_t *rp);
+int parse_uri(char *uri, char *filename, char *cgiargs);
+void serve_static(int fd, char *filename, int filesize);
+void get_filetype(char *filename, char *filetype);
+void serve_dynamic(int fd, char *filename, char *cgiargs);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
+                 char *longmsg);
+
+#endif
