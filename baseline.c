1ec8c9f1c53bad28c0f602be99f911dfbea5aa82
Jiayu Liu <jimexist@gmail.com>
2015-01-15 14:44:38 -0500

    getting rid of Rio with rio

--- baseline.c
+++ baseline.c
@@ -39,13 +39,17 @@ void serve_static(int fd, char *filename, int filesize) {
     sprintf(buf, "%sServer: Concurrent Web Server\r\n", buf);
     sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
     sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
-    Rio_writen(fd, buf, strlen(buf));
+    if (-1 == rio_writen(fd, buf, strlen(buf))) {
+        return;
+    }
 
     /* Send response body to client */
     srcfd = Open(filename, O_RDONLY, 0);
     srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
     Close(srcfd);
-    Rio_writen(fd, srcp, filesize);
+    if (-1 == rio_writen(fd, srcp, filesize)) {
+        /* broken pipe */
+    }
     Munmap(srcp, filesize);
 }
 
@@ -57,9 +61,13 @@ void serve_dynamic(int fd, char *filename, char *cgiargs) {
 
     /* Return first part of HTTP response */
     sprintf(buf, "HTTP/1.0 200 OK\r\n");
-    Rio_writen(fd, buf, strlen(buf));
+    if (-1 == rio_writen(fd, buf, strlen(buf))) {
+        return;
+    }
     sprintf(buf, "Server: Concurrent Web Server\r\n");
-    Rio_writen(fd, buf, strlen(buf));
+    if (-1 == rio_writen(fd, buf, strlen(buf))) {
+        return;
+    }
 
     if (Fork() == 0) { /* child */
         /* Real server would set all CGI vars here */
