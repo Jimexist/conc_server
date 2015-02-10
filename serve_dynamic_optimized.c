7072c29fd6f7dd5dc6f0b730bcd04decccabdec9
Jiayu Liu <jimexist@gmail.com>
2015-02-10 16:24:53 -0500

    pthread lock done

--- serve_dynamic_optimized.c
+++ serve_dynamic_optimized.c
@@ -5,38 +5,79 @@
 
 typedef void (*fp)(int fd, const char *);
 
+static int lock_read(pthread_rwlock_t *rwlock) {
+    int retval = pthread_rwlock_rdlock(rwlock);
+#ifdef DEBUG
+    printf("[%p] pthread_rwlock_rdlock/%d\n", pthread_self(), retval);
+#endif
+    if (retval) {
+        fprintf(stderr, "failed to pthread_rwlock_rdlock, errno %d\n", retval);
+    }
+    return retval;
+}
+
+static int lock_write(pthread_rwlock_t *rwlock) {
+    int retval = pthread_rwlock_wrlock(rwlock);
+#ifdef DEBUG
+    printf("[%p] pthread_rwlock_wrlock/%d\n", pthread_self(), retval);
+#endif
+    if (retval) {
+        fprintf(stderr, "failed to pthread_rwlock_wrlock, errno %d\n", retval);
+    }
+    return retval;
+}
+
+static int unlock(pthread_rwlock_t *rwlock) {
+    int retval = pthread_rwlock_unlock(rwlock);
+#ifdef DEBUG
+    printf("[%p] pthread_rwlock_unlock/%d\n", pthread_self(), retval);
+#endif
+    if (retval) {
+        fprintf(stderr, "failed to pthread_rwlock_unlock, errno %d\n", retval);
+    }
+    return retval;
+}
+
 void serve_dynamic(int fd, char *filename, char *cgiargs, Cache *code_cache,
                    pthread_rwlock_t *rwlock) {
     char libname[MAXLINE + 1];
     if (!strstr(filename, "./cgi-bin/")) {
         return;
     }
-    snprintf(libname, MAXLINE, "./cgi-bin/lib%s.so", filename + 10);
+    snprintf(libname, MAXLINE, "./cgi-bin/lib%s.so",
+             filename + strlen("./cgi-bin/"));
 
-    if (0 == pthread_rwlock_rdlock(rwlock)) {
+    if (0 == lock_read(rwlock)) {
         void *handle = get(code_cache, libname);
         if (NULL == handle) {
-            if (0 == pthread_rwlock_wrlock(rwlock)) {
-                handle = dlopen(libname, RTLD_LAZY);
-                if (NULL != handle) {
-                    void *old_handle = put(code_cache, libname, handle);
-                    if (NULL != old_handle) {
-                        if (0 != dlclose(old_handle)) {
-                            fprintf(stderr, "failed to dlclose: %s\n",
-                                    dlerror());
+            if (0 == unlock(rwlock) && 0 == lock_write(rwlock)) {
+                handle = get(code_cache, libname);
+                if (NULL == handle) {
+                    handle = dlopen(libname, RTLD_LAZY);
+                    char *errstr = dlerror();
+                    if (errstr != NULL) {
+                        fprintf(stderr, "dlopen error %s\n", errstr);
+                    }
+                    if (NULL != handle) {
+                        void *old_handle = put(code_cache, libname, handle);
+                        if (NULL != old_handle) {
+                            dlclose(old_handle);
+                            errstr = dlerror();
+                            if (errstr != NULL) {
+                                fprintf(stderr, "dlclose error %s\n", errstr);
+                            }
                         }
                     }
                 }
-                pthread_rwlock_unlock(rwlock);
             }
         }
-        if (NULL == handle) {
+        if (NULL != handle) {
             /* clear errors */
             dlerror();
-            fp func = (fp) dlsym(handle, "func");
-            char *error = dlerror();
-            if (error != NULL) {
-                fprintf(stderr, "dynamic link error %s\n", error);
+            const fp func = (fp) dlsym(handle, "func");
+            char *errstr = dlerror();
+            if (errstr != NULL) {
+                fprintf(stderr, "dynamic link error %s\n", errstr);
             } else {
                 char buf[MAXLINE + 1];
                 snprintf(buf, MAXLINE, "HTTP/1.0 200 OK\r\n");
@@ -48,7 +89,7 @@ void serve_dynamic(int fd, char *filename, char *cgiargs, Cache *code_cache,
                     }
                 }
             }
+            unlock(rwlock);
         }
-        pthread_rwlock_unlock(rwlock);
     }
 }
