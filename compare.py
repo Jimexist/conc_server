7bbe91e09cd449f47f11b9fd8681839f80150534
Jiayu Liu <jiayul@cmu.edu>
2015-02-12 21:16:05 -0500

    add data from linux and update csv to use python3

--- compare.py
+++ compare.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import re
 import os
 import subprocess
@@ -7,11 +7,11 @@ CGI_PROGRAMS = ('adder', 'multiplier', 'time', 'rand', 'sqrt')
 SERVERS = ('./baseline', './optimized')
 
 class Suite:
-    
+
     def __init__(self, conc_level, n_req):
         self.conc_level = conc_level
         self.n_req = n_req
-    
+
     def run(self):
         port = 10003
         for server in SERVERS:
@@ -21,12 +21,11 @@ class Suite:
                     'ab',
                     '-n', str(self.n_req),
                     '-c', str(self.conc_level),
-                    '-l',
                     '-r',
                     '-e', '{0}_n{1}_c{2}_result.csv'.format(server[2:], self.n_req, self.conc_level),
                     'http://{0}:{1}/cgi-bin/adder'.format('127.0.0.1', port),
                 ])
-                print output
+                print(output)
             finally:
                 p.terminate()
                 p.poll()
@@ -34,9 +33,10 @@ class Suite:
 def main():
     for server in SERVERS:
         assert os.path.exists(server), 'build binaries first'
-    
-    suite = Suite(10, 10000)
-    suite.run()
-    
+
+    for i in [1, 10, 100, 500, 1000]:
+        suite = Suite(i, 10000)
+        suite.run()
+
 if __name__ == '__main__':
     main()
